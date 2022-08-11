eff6ddd6ff519a782137bf3c9ede8a8b25e43b9a
SimpelAction <1210880461@qq.com>
2022-08-11 20:51:32 +0800

    ZZM 完成了今天的功能

--- src/filters/index.js
+++ src/filters/index.js
@@ -159,3 +159,14 @@ export function toThousandslsFilter (num) {
     .toString()
     .replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
 }
+
+export function dateForm (val) {
+  const dt = new Date(val)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+}
--- src/module-hmmm/components/articles-add.vue
+++ src/module-hmmm/components/articles-add.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class='container'>添加文章对话框</div>
+  <el-dialog
+    title="新增文章"
+    :visible="showDialog"
+    @close="btnCancel"
+  >
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="btnOK">取 消</el-button>
+      <el-button type="primary" @click="btnCancel">确 定</el-button>
+    </span>
+  </el-dialog>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    btnOK () {
+      this.$emit('update:showDialog', false)
+    },
+    btnCancel () {
+      this.$emit('update:showDialog', false)
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="scss"></style>
