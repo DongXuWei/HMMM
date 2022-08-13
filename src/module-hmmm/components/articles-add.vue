90c1df97e4a2fe7f6a2d0a2aff03ed6ae8952a78
SimpelAction <1210880461@qq.com>
2022-08-13 19:38:38 +0800

    完成了  zzm

--- src/module-hmmm/components/articles-add.vue
+++ src/module-hmmm/components/articles-add.vue
@@ -1,18 +1,29 @@
 <template>
-  <el-dialog
-    title="新增文章"
-    :visible="showDialog"
-    @close="btnCancel"
-  >
+  <el-dialog :title="`${showText}文章`" :visible="showDialog" @close="btnCancel">
+    <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+      <el-form-item label="文章标题：" prop="title">
+        <el-input v-model="formData.title"/>
+      </el-form-item>
+      <el-form-item label="文章内容：" prop="articleBody">
+        <richText v-model="formData.articleBody"/>
+      </el-form-item>
+      <el-form-item label="视频地址：" prop="videoURL">
+        <el-input v-model="formData.videoURL"/>
+      </el-form-item>
+    </el-form>
     <span slot="footer" class="dialog-footer">
-      <el-button @click="btnOK">取 消</el-button>
-      <el-button type="primary" @click="btnCancel">确 定</el-button>
+      <el-button @click="btnCancel">取 消</el-button>
+      <el-button type="primary"  @click="btnOK">确 定</el-button>
     </span>
   </el-dialog>
 </template>
 
 <script>
+import { detail, update, add } from '../../api/hmmm/articles'
+import richText from './rich-text.vue'
+
 export default {
+  components: { richText },
   props: {
     showDialog: {
       type: Boolean,
@@ -21,17 +32,54 @@ export default {
   },
   data () {
     return {
-
+      formData: {
+        title: '',
+        articleBody: '',
+        videoURL: '',
+        id: '',
+        visits: '0',
+        username: '超级管理员',
+        state: ''
+      },
+      rules: {
+        title: [{ required: true, message: '请输入文章标题', trigger: 'blur' }],
+        articleBody: [{ required: true, message: '请输入文章内容', trigger: 'blur' }]
+      }
     }
   },
   computed: {
-
+    showText () {
+      return this.formData.id ? '编辑' : '新增'
+    }
   },
   methods: {
+    async getDetail (data) {
+      const obj = await detail(data)
+      // console.log(obj)
+      this.formData = obj.data
+    },
     btnOK () {
-      this.$emit('update:showDialog', false)
+      this.$refs.perForm.validate().then(() => {
+        if (this.formData.id) {
+          return update(this.formData)
+        }
+        return add(this.formData)
+      }).then(() => {
+        this.$emit('addArt')
+        this.$message.success(`${this.formData.id ? '编辑' : '新增'}成功`)
+        this.$emit('update:showDialog', false)
+      })
     },
     btnCancel () {
+      this.formData = {
+        title: '',
+        articleBody: '',
+        videoURL: '',
+        id: '',
+        visits: '0',
+        username: '超级管理员'
+      }
+      this.$refs.perForm.resetFields()
       this.$emit('update:showDialog', false)
     }
   }
--- src/module-hmmm/components/articles-preview.vue
+++ src/module-hmmm/components/articles-preview.vue
@@ -1,9 +1,66 @@
 <template>
-  <div class='container'>预览文章对话框</div>
+  <el-dialog
+    title="预览文章"
+    :visible="showdialogVisible"
+    @close="btnCancel"
+  >
+  <h2>{{formData.title}}</h2>
+  <div class="bar">
+    <span>{{formData.createTime | dateForm}}</span>
+    <span>{{formData.username}}</span>
+    <span class="el-icon-view"></span>
+    <span>{{formData.visits}}</span>
+  </div>
+  <div class="content" v-html="formData.articleBody">{{formData.articleBody}}</div>
+  </el-dialog>
 </template>
 
 <script>
-export default {}
+import { detail } from '../../api/hmmm/articles'
+export default {
+  props: {
+    showdialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      formData: {
+        title: '',
+        articleBody: '',
+        id: '',
+        visits: '',
+        username: '',
+        creatTime: ''
+      }
+    }
+  },
+  methods: {
+    async getDetail (data) {
+      const obj = await detail(data)
+      this.formData = obj.data
+    },
+    btnCancel () {
+      this.$emit('update:showdialogVisible', false)
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang="scss">
+h2 {
+  margin: 0;
+}
+.bar {
+  height: 50px;
+  span {
+    margin-left: 10px;
+    line-height: 50px;
+  }
+}
+.content {
+  background-color: #f5f5f5;
+  border-top: 1px dashed #ccc;
+}
+</style>
